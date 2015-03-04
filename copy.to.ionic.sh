cc6ca5913031706de9d4cb317a3163e04eb59815
Norman Jarvis <norman.jarvis@gmail.com>
2015-03-03 17:39:00 -0700

    Added the pouchdb dependency in the bower.json file

--- copy.to.ionic.sh
+++ copy.to.ionic.sh
@@ -3,3 +3,4 @@
 cp -frp www/js/*.js ../turbo-vets.ionic/www/js
 cp -frp www/templates/*.html ../turbo-vets.ionic/www/templates
 cp -f www/index.html ../turbo-vets.ionic/www/
+cp -f bower.json ../turbo-vets.ionic/bower.json
