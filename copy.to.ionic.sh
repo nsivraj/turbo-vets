10635010743257b87b7a730d703ea3174ff452b9
Norman Jarvis <norman.jarvis@gmail.com>
2015-03-19 17:10:35 -0600

    Changed the name of the project from turbo-vets to turbo-vets.git so that the ionic project can remain named turbo-vets

--- copy.to.ionic.sh
+++ copy.to.ionic.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -frp www/js/*.js ../turbo-vets.ionic/www/js
-cp -frp www/templates/*.html ../turbo-vets.ionic/www/templates
-cp -f www/index.html ../turbo-vets.ionic/www/
-cp -f bower.json ../turbo-vets.ionic/bower.json
+cp -frp www/js/*.js ../turbo-vets/www/js
+cp -frp www/templates/*.html ../turbo-vets/www/templates
+cp -f www/index.html ../turbo-vets/www/
+cp -f bower.json ../turbo-vets/bower.json
