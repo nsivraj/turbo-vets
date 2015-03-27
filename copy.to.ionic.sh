1149d7648a46e120ed7ac785b696c67d6a47788d
Norman Jarvis <norman.jarvis@gmail.com>
2015-03-26 18:14:54 -0600

    Added a .project file for the ionic turbo-vets project

--- copy.to.ionic.sh
+++ copy.to.ionic.sh
@@ -4,3 +4,4 @@ cp -frp www/js/*.js ../turbo-vets/www/js
 cp -frp www/templates/*.html ../turbo-vets/www/templates
 cp -f www/index.html ../turbo-vets/www/
 cp -f bower.json ../turbo-vets/bower.json
+cp -f .project.turbo-vets ../turbo-vets/.project
